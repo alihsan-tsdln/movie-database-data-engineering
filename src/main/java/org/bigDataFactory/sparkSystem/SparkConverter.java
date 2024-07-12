bbb0e2419170574e59991bf8ea3b0a4c55144dd7
Ali Ihsan TASDELEN <alihsan.tsdln@gmail.com>
2024-07-12 12:19:52 +0300

    Added class to convert JSON data to Spark Dataframe structured data

--- src/main/java/org/bigDataFactory/sparkSystem/SparkConverter.java
+++ src/main/java/org/bigDataFactory/sparkSystem/SparkConverter.java
@@ -1,91 +1,55 @@
 package org.bigDataFactory.sparkSystem;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.spark.sql.*;
-import org.apache.spark.sql.types.DataType;
 import org.apache.spark.sql.types.DataTypes;
 import org.apache.spark.sql.types.StructType;
+import org.jetbrains.annotations.NotNull;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.noggit.JSONParser;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-
-import static org.apache.spark.sql.functions.*;
+import java.util.*;
 
 public class SparkConverter {
+
+    SparkSession spark;
+
     public SparkConverter() {
-        SparkSession spark = SparkSession.builder().master("local").getOrCreate();
+        spark = SparkSession.builder().master("local").getOrCreate();
 
         Dataset<Row> df = spark.read().option("header",true).csv("src/main/resources/Movies/credits.csv");
 
+        StructType schemaCast = new StructType()
+                .add("cast_id", DataTypes.IntegerType, false)
+                .add("character", DataTypes.StringType, true)
+                .add("gender", DataTypes.IntegerType, true)
+                .add("credit_id", DataTypes.StringType, true)
+                .add("name", DataTypes.StringType, true)
+                .add("profile_path", DataTypes.StringType, true)
+                .add("id", DataTypes.IntegerType, true)
+                .add("order", DataTypes.IntegerType, true)
+                .add("movie_id", DataTypes.StringType);
+
+        StructType schemaCrew = new StructType()
+                .add("gender", DataTypes.IntegerType)
+                .add("credit_id", DataTypes.StringType)
+                .add("name", DataTypes.StringType)
+                .add("profile_path", DataTypes.StringType)
+                .add("id", DataTypes.IntegerType)
+                .add("department", DataTypes.StringType)
+                .add("job", DataTypes.StringType)
+                .add("movie_id", DataTypes.StringType);
 
 
-        String jsonString;
-
-        List<Row> listOfMovies = df.select("cast").collectAsList();
-        int counter = 0;
-        for(Row cast : listOfMovies) {
-            jsonString = cast.getString(0);
-            try {
-            JSONArray jsonArray = createJsonDataAsArray(jsonString);
-
-            for (int i = 0; i < jsonArray.length(); i++) {
-                JSONObject jsonObject = jsonArray.getJSONObject(i);
-                //showJsonObjectData(jsonObject);
-            }
-            }
-            catch (JSONException e) {
-                System.out.println(jsonString);
-                e.printStackTrace();
-                System.out.println(++counter);
-            }
-        }
-
-        listOfMovies = df.select("crew").collectAsList();
-        System.out.println(listOfMovies.get(0).getString(0));
-        counter = 0;
-        for(int i = 0; i < listOfMovies.size(); i++) {
-            jsonString = listOfMovies.get(i).getString(0);
-            try {
-                JSONArray jsonArray = createJsonDataAsArray(jsonString);
-
-                for (int j = 0; j < jsonArray.length(); j++) {
-                    JSONObject jsonObject = jsonArray.getJSONObject(j);
-                    //showJsonCrewObjectData(jsonObject);
-                }
-            }
-            catch (JSONException e) {
-                System.out.println(++counter);
-            }
-        }
-
-        listOfMovies = df.select("id").collectAsList();
-        df.select("id").show(false);
-        /*counter = 0;
-        for(Row cast : listOfCast) {
-            jsonString = cast.getString(0);
-            try {
-                JSONArray jsonArray = createJsonDataAsArray(jsonString);
-
-                for (int i = 0; i < jsonArray.length(); i++) {
-                    JSONObject jsonObject = jsonArray.getJSONObject(i);
-                    //showJsonCrewObjectData(jsonObject);
-                }
-            }
-            catch (JSONException e) {
-                System.out.println(++counter);
-            }
-        }*/
+        Dataset<Row> dfCast = createDataset(df, "cast", schemaCast);
+        Dataset<Row> dfCrew = createDataset(df, "crew", schemaCrew);
 
 
-        //df.select("cast").show(false);
-        //df.select("crew").show(false);
+        dfCast.printSchema();
+        dfCast.show();
+
+        dfCrew.printSchema();
+        dfCrew.show();
 
         spark.stop();
         
@@ -93,47 +57,28 @@ public class SparkConverter {
         //df.show();
     }
 
-    public JSONArray createJsonDataAsArray(String jsonString) {
-        return new JSONArray(jsonString);
+
+    public Row createRowfromFactory(@NotNull JSONObject object, String id) {
+        Iterator<String> it = object.keys();
+        List<Object> valueOfRow = new ArrayList<>();
+        while (it.hasNext())
+            valueOfRow.add(object.get(it.next()));
+        valueOfRow.add(id);
+        return RowFactory.create(valueOfRow.toArray());
     }
 
-    public void showJsonCastObjectData(JSONObject jsonObject) {
-        String castId = jsonObject.getString("cast_id");
-        String character = jsonObject.getString("character");
-        String creditId = jsonObject.getString("credit_id");
-        int gender = jsonObject.getInt("gender");
-        int id = jsonObject.getInt("id");
-        String name = jsonObject.getString("name");
-        int order = jsonObject.getInt("order");
-        String profilePath = jsonObject.optString("profile_path", "N/A");
 
-        System.out.println("Cast ID: " + castId);
-        System.out.println("Character: " + character);
-        System.out.println("Credit ID: " + creditId);
-        System.out.println("Gender: " + gender);
-        System.out.println("ID: " + id);
-        System.out.println("Name: " + name);
-        System.out.println("Order: " + order);
-        System.out.println("Profile Path: " + profilePath);
-        System.out.println();
-    }
-
-    public void showJsonCrewObjectData(JSONObject jsonObject) {
-        String creditId = jsonObject.getString("credit_id");
-        String department = jsonObject.getString("department");
-        int gender = jsonObject.getInt("gender");
-        int id = jsonObject.getInt("gender");
-        String job = jsonObject.getString("job");
-        String name = jsonObject.getString("name");
-        String profilePath = jsonObject.optString("profile_path", "N/A");
-
-        System.out.println("Credit ID: " + creditId);
-        System.out.println("Department: " + department);
-        System.out.println("Gender: " + gender);
-        System.out.println("ID: " + id);
-        System.out.println("Job: " + job);
-        System.out.println("Name: " + name);
-        System.out.println("Profile Path: " + profilePath);
-        System.out.println();
+    public Dataset<Row> createDataset(Dataset<Row> df, String columnName, StructType schema) {
+        int movieIdx = 0;
+        List<Row> listOfData = df.select(columnName).collectAsList();
+        List<Row> movie_ids = df.select("id").collectAsList();
+        ArrayList<Row> moviesDataList = new ArrayList<>();
+        for(Row cast : listOfData) {
+            JSONArray jsonArray = new JSONArray(cast.getString(0));
+            for (int i = 0; i < jsonArray.length(); i++)
+                moviesDataList.add(createRowfromFactory(jsonArray.getJSONObject(i), movie_ids.get(movieIdx).getString(0)));
+            movieIdx++;
+        }
+        return spark.createDataFrame(moviesDataList, schema);
     }
 }
